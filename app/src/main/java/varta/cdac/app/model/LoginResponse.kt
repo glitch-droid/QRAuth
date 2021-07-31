4f93881e5b5aefbbf6e8fc5576c37f997d9b2afb
Elvis Rahman <elvisrahman4@gmail.com>
2021-07-31 12:35:28 +0530

    Webservices Fixed (Getting Correct response)

---
+++ app/src/main/java/varta/cdac/app/model/LoginResponse.kt
@@ -0,0 +1,9 @@
+package varta.cdac.app.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+    var QRtoken: String
+
+)
