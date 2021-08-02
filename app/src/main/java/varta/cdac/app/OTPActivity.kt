3e182ac30aa724dc0999a3d86891ac435bc93da6
Elvis Rahman <elvisrahman4@gmail.com>
2021-08-02 20:33:27 +0530

    Otp session timeout added Fixed

---
+++ app/src/main/java/varta/cdac/app/OTPActivity.kt
@@ -0,0 +1,51 @@
+package varta.cdac.app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.cardview.widget.CardView
+import java.util.*
+
+class OTPActivity : AppCompatActivity() {
+    private var editTextOtp : EditText? = null
+    private var loginBtn : Button? = null
+    private val timer = Timer()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+
+        editTextOtp = findViewById(R.id.edit_text_otp)
+        loginBtn = findViewById(R.id.login_btn)
+        startUserSession();
+        loginBtn!!.setOnClickListener{
+            timer.cancel()
+            val intent = Intent(this@OTPActivity,HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    private fun startUserSession() {
+        timer.schedule(object : TimerTask(){
+            override fun run() {
+                handleSessionExpired()
+            }
+
+        }, 10000)
+
+    }
+
+    private fun handleSessionExpired() {
+        timer.cancel()
+        val intent = Intent(this@OTPActivity,MainActivity::class.java);
+        startActivity(intent)
+        finish()
+
+    }
+}
